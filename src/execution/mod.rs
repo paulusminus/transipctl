494db152f595decb2599bf04f5f4a167658c83a9
paulusminus <info@paulmin.nl>
2023-11-02 07:04:23 +0100

    extension and_then_json

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -19,8 +19,14 @@ use crate::command::{
     TransipCommand,
 };
 
-fn to_json<T: Serialize>(t: T) -> Result<String, transip::Error> {
-    serde_json::to_string_pretty(&t).map_err(transip::Error::from)
+pub trait ToJson {
+    fn and_then_json(self) -> Result<String, transip::Error>;
+}
+
+impl<T: Serialize> ToJson for Result<T, transip::Error> {
+    fn and_then_json(self) -> Result<String, transip::Error> {
+        self.and_then(|t| serde_json::to_string_pretty(&t).map_err(transip::Error::from))
+    }
 }
 
 pub trait Execution {
@@ -30,7 +36,7 @@ pub trait Execution {
 impl Execution for TransipCommand {
     fn execute(&self, client: &mut Client) -> Result<String, transip::Error> {
         match self {
-            Self::Comment(_comment) => to_json(()),
+            Self::Comment(_comment) => Ok(()).and_then_json(),
             Self::Dns(command) => command.execute(client),
             Self::Domain(command) => command.execute(client),
             Self::Invoice(command) => command.execute(client),
@@ -45,9 +51,9 @@ impl Execution for DnsCommand {
         match self {
             Self::DeleteAcmeChallenge(name) => {
                 DnsApi::dns_entry_delete_all(client, name, DnsEntry::is_acme_challenge)
-                    .and_then(to_json)
+                    .and_then_json()
             }
-            Self::List(name) => DnsApi::dns_entry_list(client, name).and_then(to_json),
+            Self::List(name) => DnsApi::dns_entry_list(client, name).and_then_json(),
             Self::SetAcmeChallenge(name, value) => {
                 DnsApi::dns_entry_delete_all(client, name, DnsEntry::is_acme_challenge)
                     .and_then(|_| {
@@ -57,7 +63,7 @@ impl Execution for DnsCommand {
                             DnsEntry::new_acme_challenge(60, value),
                         )
                     })
-                    .and_then(to_json)
+                    .and_then_json()
             }
         }
     }
@@ -66,8 +72,8 @@ impl Execution for DnsCommand {
 impl Execution for DomainCommand {
     fn execute(&self, client: &mut Client) -> Result<String, transip::Error> {
         match self {
-            Self::Item(name) => client.domain_item(name).and_then(to_json),
-            Self::List => client.domain_list().and_then(to_json),
+            Self::Item(name) => client.domain_item(name).and_then_json(),
+            Self::List => client.domain_list().and_then_json(),
         }
     }
 }
@@ -76,10 +82,10 @@ impl Execution for InvoiceCommand {
     fn execute(&self, client: &mut Client) -> Result<String, transip::Error> {
         match self {
             Self::Action(name, action) => match action {
-                InvoiceAction::Item => client.invoice(name).and_then(to_json),
-                InvoiceAction::Pdf => client.invoice_pdf(name).and_then(to_json),
+                InvoiceAction::Item => client.invoice(name).and_then_json(),
+                InvoiceAction::Pdf => client.invoice_pdf(name).and_then_json(),
             },
-            Self::List => client.invoice_list().and_then(to_json),
+            Self::List => client.invoice_list().and_then_json(),
         }
     }
 }
@@ -87,28 +93,8 @@ impl Execution for InvoiceCommand {
 impl Execution for ProductCommand {
     fn execute(&self, client: &mut Client) -> Result<String, transip::Error> {
         match self {
-            Self::Elements(elements) => client.product_elements(elements).and_then(to_json),
-            Self::List => client.products().and_then(to_json),
-        }
-    }
-}
-
-pub trait JsonReport {
-    fn json_report(self);
-}
-
-impl<T: Serialize> JsonReport for std::result::Result<T, transip::Error> {
-    fn json_report(self) {
-        match self {
-            Ok(t) => {
-                let s = serde_json::to_string_pretty(&t).unwrap();
-                if s != "null" {
-                    println!("{}", s);
-                }
-            }
-            Err(error) => {
-                eprintln!("Error: {}", error);
-            }
+            Self::Elements(elements) => client.product_elements(elements).and_then_json(),
+            Self::List => client.products().and_then_json(),
         }
     }
 }
@@ -117,14 +103,14 @@ impl Execution for VpsCommand {
     fn execute(&self, client: &mut Client) -> Result<String, transip::Error> {
         match self {
             Self::Action(name, action) => match action {
-                VpsAction::Item => client.vps_list().and_then(to_json),
-                VpsAction::Lock => client.vps_set_is_locked(name, true).and_then(to_json),
-                VpsAction::Reset => client.vps_reset(name).and_then(to_json),
-                VpsAction::Start => client.vps_start(name).and_then(to_json),
-                VpsAction::Stop => client.vps_stop(name).and_then(to_json),
-                VpsAction::Unlock => client.vps_set_is_locked(name, false).and_then(to_json),
+                VpsAction::Item => client.vps_list().and_then_json(),
+                VpsAction::Lock => client.vps_set_is_locked(name, true).and_then_json(),
+                VpsAction::Reset => client.vps_reset(name).and_then_json(),
+                VpsAction::Start => client.vps_start(name).and_then_json(),
+                VpsAction::Stop => client.vps_stop(name).and_then_json(),
+                VpsAction::Unlock => client.vps_set_is_locked(name, false).and_then_json(),
             },
-            Self::List => client.vps_list().and_then(to_json),
+            Self::List => client.vps_list().and_then_json(),
         }
     }
 }
